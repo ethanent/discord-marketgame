dc9fa5e4c688a565a702272dd66a958ce54ba6fc
Sa'ar <geger40.1@gmail.com>
2020-02-26 22:01:11 -0800

    Added panic orders (#1)

---
+++ c_panic.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func cmdPanic(s *discordgo.Session, m *discordgo.Message, args []string) error {
+	if len(args) < 1 {
+		return errors.New("Too few arguments.\nUsage: !panic <symbol>")
+	}
+	
+	symbol := strings.ToUpper(args[0])
+	
+	u, e := GetUser(m.Author.ID)
+	
+	if e != nil {
+		return e
+	}
+	
+	shares, ok := u.Shares[symbol]
+	
+	if !ok {
+		return errors.New("You don't own any " + symbol + ".")
+	}
+
+	return cmdSell(s, m, []string{symbol, strconv.Itoa(shares)})
+}
